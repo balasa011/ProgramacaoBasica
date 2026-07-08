dd55fe301768eb7120a6aae62c0d2f5bc59b510c
balasa011 <guilhermebalasa011@gmail.com>
2026-07-08 14:17:30 -0300

    fix= erro encontrado no codigo

--- solucoes/src/main/java/org/example/MaquinaDeCafe.java
+++ solucoes/src/main/java/org/example/MaquinaDeCafe.java
@@ -11,29 +11,10 @@ public class MaquinaDeCafe {
         int b = 0;
         int c = 0;
         int d = 0;
+        if (){
 
-        if (a1 < a2 || a1 < a3) {
-            b = b + a1;
+
+
         }
-        if (a2 < a1 || a2 < a3) {
-            c = c + a2;
-        }
-        if (a3 < a1 || a3 < a2) {
-            d = d + a3;
-        }
-        if (b == 0){
-            c= c *2;
-            d= d*4;
-        }
-        if (c == 0){
-            b= b*2;
-            d= d*2;
-        }
-        if (d == 0){
-            c= c*2;
-            b= b*4;
-        }
-        int e = (b + c + d);
-        System.out.println(e);
     }
 }
